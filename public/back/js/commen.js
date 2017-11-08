b197ae5641deb7f92f7a304b3b2810106c0953bd
fanjun-junyi <2574938931@qq.com>
2017-11-08 21:37:59 +0800

    添加进度条效果

--- public/back/js/commen.js
+++ public/back/js/commen.js
@@ -2,6 +2,17 @@
 * @Author: fanjun-junyi
 * @Date:   2017-11-08 10:58:12
 * @Last Modified by:   fanjun-junyi
-* @Last Modified time: 2017-11-08 14:20:49
+* @Last Modified time: 2017-11-08 21:34:20
 */
 
+
+$(document).ajaxStart(function() {
+  /* Stuff to do when an AJAX call is started and no other AJAX calls are in progress */;
+  NProgress.start();
+});
+$(document).ajaxStop(function() {
+  /* stuff to do when all AJAX calls have completed */;
+  setTimeout(function(){
+    NProgress.done();
+  },500)
+});
