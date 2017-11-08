17b6246c5a8f44dcaef0e7175b25af360ac9989b
fanjun-junyi <2574938931@qq.com>
2017-11-08 11:01:28 +0800

    登录架构

---
+++ public/back/js/commen.js
@@ -0,0 +1,7 @@
+/*
+* @Author: fanjun-junyi
+* @Date:   2017-11-08 10:58:12
+* @Last Modified by:   fanjun-junyi
+* @Last Modified time: 2017-11-08 10:59:01
+*/
+
---
+++ public/back/js/login.js
@@ -0,0 +1,7 @@
+/*
+* @Author: fanjun-junyi
+* @Date:   2017-11-08 11:00:14
+* @Last Modified by:   fanjun-junyi
+* @Last Modified time: 2017-11-08 11:00:21
+*/
+
